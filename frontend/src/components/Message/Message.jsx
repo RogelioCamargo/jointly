5dc0572a718ac01450882333df4a9365c62dcefa
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-08-07 08:28:01 -0700

    Create message component

---
+++ frontend/src/components/Message/Message.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Message = ({ src, name, content, timestamp }) => {
+	return (
+		<div className="px-5">
+			<div className="flex mb-3">
+				<div className="h-9 w-9 mr-2 overflow-hidden rounded-md ">
+					<img className="object-fill" src={src} alt="" />
+				</div>
+				<div className="w-3/4">
+					<p className="font-bold">{name}<span className="text-sm text-slate-500 ml-1 font-normal">{timestamp}</span></p>
+					<p className="text-sm">{content}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Message;
