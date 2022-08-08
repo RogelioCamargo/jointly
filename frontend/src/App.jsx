9b667ce35b938d80a9aa578052d25831f6fa6820
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-08-08 08:40:27 -0700

    Add input element for search in header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,12 @@ function App() {
   return (
     <div className="App">
 			<div className="bg-green-500 h-12 w-full">
-				<div className="flex justify-end items-center h-full px-5">
-					<div className="h-8 w-8 mr-2 overflow-hidden rounded-full ">
-						<img className="object-fill" src="https://image13.photobiz.com/9048/3_20180601100200_22858587_large.jpg" alt="" />
+				<div className="relative w-full h-12">
+					<div className="flex justify-end items-center h-full px-5">
+						<input className="bg-green-400 text-slate-200 placeholder-slate-200 rounded-md py-1 px-2 w-1/4 absolute left-0 right-0 mr-auto ml-auto" type="text" name="" id="" placeholder="Search" />
+						<div className="h-8 w-8 mr-2 overflow-hidden rounded-full ">
+							<img className="object-fill" src="https://image13.photobiz.com/9048/3_20180601100200_22858587_large.jpg" alt="" />
+						</div>
 					</div>
 				</div>
 			</div>
