a449d5c96c413dfec188ad3ab99f9ec6dad791df
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-08-09 07:57:08 -0700

    Create header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import CompanyDiscussion from "./components/CompanyDiscussion";
 import SideMenu from "./components/SideMenu";
 import TicketList from "./components/TicketList";
 import TicketListItem from "./components/TicketListItem/";
+import Header from "./components/Header";
 
 function App() {
 	const ticket = {
@@ -18,34 +19,12 @@ function App() {
 
 	return (
 		<div className="App">
-			<div className="bg-green-500 h-12 w-full">
-				<div className="relative w-full h-12">
-					<div className="flex justify-end items-center h-full px-5">
-						<input
-							className="bg-green-400 text-white placeholder-white rounded-md py-1 px-2 w-1/4 absolute left-0 right-0 mr-auto ml-auto"
-							type="text"
-							name=""
-							id=""
-							placeholder="Search"
-						/>
-						<div className="flex items-center">
-							<span className="text-white mr-2">Angela Stevens</span>
-							<div className="h-8 w-8 mr-2 overflow-hidden rounded-full bg-black">
-								<img
-									className="object-fill"
-									src="https://i.pinimg.com/736x/cb/aa/e7/cbaae7e35ef288b69c8d6e16589eac04.jpg"
-									alt=""
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Header />
 			<div className="flex w-full h-full">
 				<SideMenu />
 				<main className="main-content">
 					<div className="h-full">
-						{/* <TicketList /> */}
+						<TicketList />
 						<div className="py-3 h-full">
 							<div className="px-5">
 								<TicketListItem {...ticket} />
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className="bg-green-500 h-12 w-full">
+			<div className="relative w-full h-12">
+				<div className="flex justify-end items-center h-full px-5">
+					<input
+						className="bg-green-400 text-white placeholder-white rounded-md py-1 px-2 w-1/4 absolute left-0 right-0 mr-auto ml-auto"
+						type="text"
+						name=""
+						id=""
+						placeholder="Search"
+					/>
+					<div className="flex items-center">
+						<span className="text-white mr-2">Angela Stevens</span>
+						<div className="h-8 w-8 mr-2 overflow-hidden rounded-full bg-black">
+							<img
+								className="object-fill"
+								src="https://i.pinimg.com/736x/cb/aa/e7/cbaae7e35ef288b69c8d6e16589eac04.jpg"
+								alt=""
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
